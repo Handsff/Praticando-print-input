cb19b963a448b2f580f4c6e06655efb2e1d801b3
Daniel Campos <tattusop@gmail.com>
2021-05-16 16:38:03 -0300

    Create Questionario.py
    Aceito dicas de como simplificar o script.

---
+++ Questionario.py
@@ -0,0 +1,14 @@
+#Praticando print / input
+
+print('Ola, diga a data de nasc:')
+
+dia= input('Dia: ' )
+mes= input('Mes: ' )
+ano= input('Ano: ' )
+
+ok= ('Você nasceu no dia')
+ok1= ('de')
+ok2= ('de')
+ok3=('. Correto')
+
+print(ok, dia, ok1, mes,ok2, ano, ok3)
